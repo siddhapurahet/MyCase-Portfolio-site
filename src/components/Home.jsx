ab18502153492fd785fbf0e5d898a26146d2d716
Het Siddhapura <het@aarogram.com>
2024-02-09 15:53:53 +0530

    changes for project section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
         </p>
         <div>
           <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
-            <Link to="work">View Projects</Link>
+            <Link to="project">View Projects</Link>
             <span className="group-hover:rotate-90 duration-300">
               <HiArrowNarrowRight className="ml-3 " />
             </span>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -24,7 +24,10 @@ const Projects = () => {
   const singleProject = projects[currentIndex];
 
   return (
-    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+    <div
+      name="project"
+      className="w-full md:h-screen text-gray-300 bg-[#0a192f]"
+    >
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
